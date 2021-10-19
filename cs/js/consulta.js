e58a1233283a6f4932cea780f6917cd594dba5d5
MaxAim <fakku_n@hotmail.com>
2021-10-19 12:32:51 -0300

    Entrega

---
+++ cs/js/consulta.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	if ($("#consulta").is(":empty")){
+        $("#consulta").html('<object style="width: 56%; height:1300px; margin-bottom: 5%;" data="https://docs.google.com/forms/d/e/1FAIpQLSfiysg9VQOATjOuXlk20ApNEB2C3tezqH_uUOElNTk5Wv3PsQ/viewform?ts=609d3a05">');
+		$(".freebirdLightBackground ").css("background-color", "white")
+  	}
+})
